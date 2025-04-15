96898af8e7a9829c847c9a19d4f34da803527997
DemZap <demzapo@gmail.com>
2025-04-15 14:29:50 +0300

    commi second

--- lesson3/homework3.1.py
+++ lesson3/homework3.1.py
@@ -2,16 +2,16 @@ num1 = int(input("Введіть число: "))
 operation = input("Зробіть операцію  (+, -, *, /): ")
 num2 = int(input("Введіть друге число: "))
 if operation == "+":
-        result = num1 + num2
-        print(result)
+    result = num1 + num2
+    print(result)
 elif operation == "-":
-        result = num1 - num2
-        print(result)
+    result = num1 - num2
+    print(result)
 elif operation == "*":
-        result = num1 * num2
-        print(result)
+    result = num1 * num2
+    print(result)
 elif num2 == 0:
-        print("Помилка: Ділення на нуль неможно!")
+    print("Помилка: Ділення на нуль неможно!")
 elif operation == "/":
-        result = num1 / num2
-        print(result)
+    result = num1 / num2
+    print(result)
