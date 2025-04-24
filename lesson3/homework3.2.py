f7baa7b76f2bc79e079e049f9dfcfca4944f80cd
DemZap <demzapo@gmail.com>
2025-04-24 17:18:20 +0300

    delet

--- lesson3/homework3.2.py
+++
@@ -1,7 +0,0 @@
-lst = [12, 13, 47, 110, 21]
-lst.remove(21)
-lst.insert(0, 21)
-print(lst)
-
-
-
--- lesson4/homework4.3.py
+++
@@ -1,8 +0,0 @@
-import random
-
-lst = random.sample(range(1, 100), random.randint(3, 10))
-new_lst = [lst[0], lst[2], lst[-2]]
-
-print("Початковий список:", lst)
-print("Новий список:", new_lst)
-
