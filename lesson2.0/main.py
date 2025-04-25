23f403d88cd4a0f3596e237037c79d5a3f402fdb
DemZap <demzapo@gmail.com>
2025-04-25 15:17:35 +0300

    dz1

---
+++ lesson2.0/main.py
@@ -0,0 +1,30 @@
+# print("Hello World!")
+#
+
+# some_string = "something \n 🫣\U00002602"
+# print(some_string)
+# pos_number_of_a = ord("a")
+#
+#
+# char_97 = chr(97)
+#
+# pass
+# print(char_97)
+# print(f"{char_97}")something
+#
+# import templates
+# import constants
+# print(id("blabla"))
+# print(id("blabla"))
+
+
+some_string = "blabla2 \n ☂  \t їїї \U00000457 \U00002602"
+print(some_string)
+pos_number_of_a = ord("a")
+pos_number_of_umbrella = ord("\U00002602")
+char_97 = chr(97)
+valid_symbol = ""
+print(char_97)
+formatted_string = f"{char_97=} but {pos_number_of_a=}"
+pass
+print(formatted_string)
