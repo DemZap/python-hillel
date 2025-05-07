e72a215675f9b7398893cbe554a2829478504349
DemZap <demzapo@gmail.com>
2025-05-07 17:38:11 +0300

    add 1 homework4

--- homework/homework4/homework4_2.py
+++ homework/homework4/homework4_2.py
@@ -1,9 +1,9 @@
-some_list = [0, 1, 7, 2, 4, 8]
-# some_list = [1, 3, 5]
+# some_list = [0, 1, 7, 2, 4, 8]
+some_list = [1, 3, 5]
 # some_list = [6]
 # some_list = []
 
-index_element = [some_list[index] for index in [0, 2, 4] if index < len(some_list)]
-some_list = sum(index_element) * some_list[-1]
+if some_list:
+    some_list = sum(some_list[::2]) * some_list[-1]
 print(some_list)
 pass
