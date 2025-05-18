011ebe11825938be3c2d03dd12a971b7d5540121
DemZap <demzapo@gmail.com>
2025-05-18 17:13:37 +0300

    add 1 homework6+

--- homework/homework6/homework6_2.py
+++ homework/homework6/homework6_2.py
@@ -1,28 +1,21 @@
-# Отримуємо введене число від користувача
 seconds = int(input("Введіть кількість секунд: "))
 
-# Обчислюємо кількість днів
-days, remaining_seconds = divmod(seconds, 86400)
+days, number_seconds = divmod(seconds, 86400)
 
-# Обчислюємо години
-hours, remaining_seconds = divmod(remaining_seconds, 3600)
+hours, number_seconds = divmod(number_seconds, 3600)
 
-# Обчислюємо хвилини
-minutes, remaining_seconds = divmod(remaining_seconds, 60)
+minutes, number_seconds = divmod(number_seconds, 60)
 
-# Останні секунди
-seconds = remaining_seconds
+seconds = number_seconds
 
-# Формуємо коректне написання слова "день/дні"
-if 10 <= days % 100 <= 20 or days % 10 >= 5 or days % 10 == 0:
-    day_word = "днів"
+if 10 <= days % 100 or days % 10 == 0:
+    day = "днів"
 elif days % 10 == 1:
-    day_word = "день"
+    day = "днів"
 else:
-    day_word = "дні"
+    day = "днів"
 
-# Форматуємо вивід
-formatted_time = f"{days} {day_word}, {str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}"
+formatted_time = f"{days} {day}, {str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}"
 
 print(formatted_time)
 pass
