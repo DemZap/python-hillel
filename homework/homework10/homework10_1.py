369c2f9b0cf97e0043f1dc5f3aa173719c4efaa1
Запорожець Дмитро <97732552+DemZap@users.noreply.github.com>
2025-06-03 17:58:59 +0300

    10

--- homework/homework10/homework10_1.py
+++ homework/homework10/homework10_1.py
@@ -1,30 +1,18 @@
 def pow(x):
     return x**2
 
-
 def some_gen(begin, end, func):
-    """
-    begin: перший елемент послідовності
-    end: кількість елементів у послідовності
-    func: функція, яка формує значення для послідовності
-    """
+
     value = begin
     for _ in range(end):
         yield value
-        value = func(value)  # Формуємо наступний член послідовності
-
+        value = func(value)
 
 from inspect import isgenerator
 
-# Тестування
 gen = some_gen(2, 4, pow)
 assert isgenerator(gen) == True, "Test1"
 assert list(gen) == [2, 4, 16, 256], "Test2"
 
-print("ОК")  # Якщо всі тести проходять, код працює правильно
+print("ОК")
 pass
-
-# ✔ Створюємо змінну value, яка приймає початкове значення begin
-# ✔ Запускаємо цикл for _ in range(end), щоб видати n членів послідовності
-# ✔ yield value повертає поточний елемент
-# ✔ value = func(value) змінює value на наступне значення за заданим законом
--- homework/homework10/homework10_2.py
+++ homework/homework10/homework10_2.py
@@ -1,12 +1,10 @@
 import re
 
-
 def first_word(text):
-    """Пошук першого слова у рядку"""
     return re.search(r"[a-zA-Z']+", text).group()
 
 
-# Тестування
+
 assert first_word("Hello world") == "Hello", "Test1"
 assert first_word("greetings, friends") == "greetings", "Test2"
 assert first_word("don't touch it") == "don't", "Test3"
@@ -14,10 +12,5 @@ assert first_word(".., and so on ...") == "and", "Test4"
 assert first_word("hi") == "hi", "Test5"
 assert first_word("Hello.World") == "Hello", "Test6"
 
-print("ОК")  # Всі тести повинні пройти
+print("ОК")
 pass
-
-# ✔ Використовує re.search() для знаходження першого слова у тексті
-# ✔ r"[a-zA-Z']+" — регулярний вираз, який шукає слово (включаючи апострофи ')
-# ✔ .group() повертає знайдене слово
-# ✔ Проходить всі тестові випадки
