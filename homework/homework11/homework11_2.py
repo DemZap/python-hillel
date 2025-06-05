39c88ae55ef10e2fc371008618242f900a52eee0
DemZap <demzapo@gmail.com>
2025-06-05 18:32:16 +0300

    Initial commit

--- homework/homework11/homework11_2.py
+++ homework/homework11/homework11_2.py
@@ -1,15 +1,13 @@
 def generate_cube_numbers(end):
-    """Генерує куби чисел, починаючи з 2 до заданого 'end'"""
     num = 2
     while True:
         cube = num**3
         if cube > end:
-            return  # Завершуємо генератор, коли куб перевищує 'end'
+            return
         yield cube
         num += 1
 
 
-# Тестування
 from inspect import isgenerator
 
 gen = generate_cube_numbers(1)
@@ -32,9 +30,5 @@ assert list(generate_cube_numbers(1000)) == [
     1000,
 ], "10 у кубі це 1000"
 
-print("ОК")  # Якщо всі тести проходять, код працює правильно
+print("ОК")
 pass
-# ✔ Цикл while True забезпечує безперервну генерацію кубів чисел.
-# ✔ cube = num ** 3 обчислює куб поточного числа.
-# ✔ Якщо куб більше end, використовується return для завершення генератора.
-# ✔ yield cube повертає наступне значення куба перед збільшенням num.
