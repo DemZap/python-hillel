0e7d61bd8dd4225439586660b80b7dc7aeee9fa5
DemZap <demzapo@gmail.com>
2025-06-08 14:07:30 +0300

    12

--- homework/homework12/homework12_2.py
+++ homework/homework12/homework12_2.py
@@ -1,34 +1,74 @@
-def generate_cube_numbers(end):
-    num = 2
-    while True:
-        cube = num**3
-        if cube > end:
-            return
-        yield cube
-        num += 1
+class Item:
+    def __init__(self, name, price, description, dimensions):
+        self.name = name
+        self.price = price
+        self.description = description
+        self.dimensions = dimensions
+
+    def __str__(self):
+        return f"{self.name}, price: {self.price}"
 
 
-from inspect import isgenerator
+class User:
+    def __init__(self, name, surname, numberphone):
+        self.name = name
+        self.surname = surname
+        self.numberphone = numberphone
 
-gen = generate_cube_numbers(1)
-assert isgenerator(gen) == True, "Test0"
-assert list(generate_cube_numbers(10)) == [8], "оскільки воно менше 10."
-assert list(generate_cube_numbers(100)) == [
-    8,
-    27,
-    64,
-], "5 у кубі це 125, а воно вже більше 100"
-assert list(generate_cube_numbers(1000)) == [
-    8,
-    27,
-    64,
-    125,
-    216,
-    343,
-    512,
-    729,
-    1000,
-], "10 у кубі це 1000"
+    def __str__(self):
+        return f"{self.name} {self.surname}"
 
-print("ОК")
-pass
+
+class Purchase:
+    def __init__(self, user):
+        self.user = user
+        self.products = {}
+
+    def add_item(self, item, cnt):
+        self.products[item] = cnt
+
+    def get_total(self):
+        return sum(item.price * cnt for item, cnt in self.products.items())
+
+    def __str__(self):
+        result = f"User: {self.user}\nItems:\n"
+        for item, cnt in self.products.items():
+            result += f"{item.name}: {cnt} pcs.\n"
+        return result.strip()
+
+
+
+lemon = Item('lemon', 5, "yellow", "small")
+apple = Item('apple', 2, "red", "middle")
+
+print(lemon)  # lemon, price: 5
+
+buyer = User("Ivan", "Ivanov", "02628162")
+print(buyer)  # Ivan Ivanov
+
+cart = Purchase(buyer)
+cart.add_item(lemon, 4)
+cart.add_item(apple, 20)
+print(cart)
+"""
+User: Ivan Ivanov
+Items:
+lemon: 4 pcs.
+apple: 20 pcs.
+"""
+
+assert isinstance(cart.user, User) is True, 'Екземпляр класу User'
+assert cart.get_total() == 60, "Всього 60"
+assert cart.get_total() == 60, 'Повинно залишатися 60!'
+
+cart.add_item(apple, 10)
+print(cart)
+"""
+User: Ivan Ivanov
+Items:
+lemon: 4 pcs.
+apple: 10 pcs.
+"""
+
+assert cart.get_total() == 40, "Повинно залишатися 40!"
+print("Усі тести пройдені!")
